a8d8131ab466948f4dfa4a7e934807639b260b45
Simon McCallum <simon.mccallum@hig.no>
2013-08-15 01:33:49 +0200

    Added a population of 200 over norway.  Then added a kill button.  Now the case just needs to be worked out so that I am using real numbers

--- index.php
+++ index.php
@@ -7,21 +7,50 @@
 		body
 		{
 			background: #efefef;
+			
+			
 		}
+		.living
+			{
+				fill:#999999;
+			}
+		.dead
+			{
+				fill:red;
+			}
+		.saved
+			{
+				fill:green;
+			}
+		.light
+			{
+				fill:#DDD;
+				stroke:#BBB;
+			}
 	</style>
 </head>
 <body>
 	<script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
 	<script src="./js/d3.geo.js" charset="utf-8"></script>
 	<script>
-		var width = 960;
+		var width = 400;
 		var height = 400;
 		var dataset = [ 5, 10, 13, 19];
+		
+		var dead = 0;
 		
 		var svg = d3.select("body").append("svg")
 		    .attr("width", width)
 		    .attr("height", height);
 		    svg.attr("id","gameview");
+		    svg.append("text")
+		    .attr("id","killtext")
+		    .attr("x","350")
+		    .attr("y","20")
+		    .attr("font-family","sans-serif")
+		    .attr("font-size","10px")
+		    .attr("fill","red")
+		    .text("Starting pop.");
 		    
 		var defs=svg.append("defs");
 		defs.append("mask");
@@ -32,9 +61,9 @@
 
 		var path = d3.geo.path()
 			.projection(d3.geo.albers()
-			.origin([12, 65])
+			.origin([18, 65])
 			.parallels( [60.0, 68.0] )
-			.scale(1000)
+			.scale(1500)
 			.translate([200, 200])
 		);
 
@@ -43,35 +72,38 @@
 			fylker.selectAll( "path" )
 				.data( json.features )
 				.enter().append("svg:path")
-				.attr("d", path);
+				.attr("d", path)
+				.attr("class","light");
 		}); 
 		
-            function make_row(name, icon, n, x, y)
+		
+            function make_row(name, icon, n, x, y, row)
             {
                 d3.select("svg").selectAll(name)
                     .data(d3.range(n))
-                .enter()
+                    .enter()
                     .append("svg:use")
                     .attr("class", name)
+                    .attr("id", function(d,i) { return "icon"+row+"_"+i})
                     .attr("xlink:href", "#" + icon + "icon")
                     .attr("transform", function(d,i) {
-                           return "translate(" + [x + 50 * i, y] + ")" + "scale("+ 0.3 +")"
+                           return "translate(" + [x + 15 * i, y] + ")" + "scale("+ 0.3 +")"
                     })
             }
             
 		
 		 
-		d3.json("test.json", function(error, data) {
-			for(var d in data)
-			{
-				console.log(data[d][1]);
-			 	svg.append("circle").attr("r", data[d][1]).attr("transform", "translate("+(20*(d+1))+",100)")
-			 	.attr("style", "fill:#00ff00;fill-opacity:1;stroke:none");
-			}
+		//d3.json("test.json", function(error, data) {
+		//	for(var d in data)
+		//	{
+		//		console.log(data[d][1]);
+		//	 	svg.append("circle").attr("r", data[d][1]).attr("transform", "translate("+(20*(d+1))+",100)")
+		//	 	.attr("style", "fill:#00ff00;fill-opacity:1;stroke:none");
+		//	}
 			// data.foreach(function(d){
 			// 	console.log(d.item[0]);
 			// });
-		});
+		//});
 		
 		function load_human(name){
 			d3.xml("./images/human.svg", "image/svg+xml", function (xml) {  
@@ -114,11 +146,45 @@
 		}); 
 		}
 
+		function randomkill(){
+			reset("dead");
+			reset("saved");
+		        var killed = 3+Math.floor(Math.random()*4);
+		        d3.select("killtext").text(killed+" died");
+		        for (var i=0; i<killed; i++)
+		        {
+				var kill_object = document.getElementById("icon"+Math.floor(Math.random()*10)+"_"+Math.floor(Math.random()*20));
+				dead += 1;
+				kill_object.setAttribute("class","dead");
+				//kill_object.setAttribute("transform","translate(" + [400+(dead *12),300] + ")" + "scale("+ 0.3 +")")
+			}
+			var saved = 3+Math.floor(Math.random()*4);
+		        for (var i=0; i<saved; i++)
+		        {
+				var kill_object = document.getElementById("icon"+Math.floor(Math.random()*10)+"_"+Math.floor(Math.random()*20));
+				dead += 1;
+				kill_object.setAttribute("class","saved");
+				//kill_object.setAttribute("transform","translate(" + [400+(dead *12),300] + ")" + "scale("+ 0.3 +")")
+			}
+			console.log(kill_object);
+		}
+		
+		function reset(classname){
+			var deadones= document.getElementsByClassName(classname);
+			for(var i=0 ; i<deadones.length;i++){
+				deadones[i].setAttribute("class","living");
+			};
+		}
+
 		load_human("human");
-		make_row("a","human",10,20,20);
+		for (var i=0;i<10;i++)
+		{
+			make_row("living","human",20,20,40*i,i);
+			};
 		
 		
 
-</script>
+</script><br\>
+<input id="clickMe" type="button" value="Run test" onclick="randomkill();" />
 </body>
 </html>
